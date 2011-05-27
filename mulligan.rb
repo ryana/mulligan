ddb5b4b481ff8db7b85019da171e2be5f925419f
Ryan Angilly <ryan@angilly.com>
2011-05-27 14:43:29 -0600

    add the example

---
+++ mulligan.rb
@@ -0,0 +1,69 @@
+module RetryHarder
+  class RetryResult
+
+    attr_accessor :success
+
+    def initialize(options)
+      self.success = options[:success]
+    end
+
+    def on_failure(&block)
+      if !success
+        block.call
+      end
+
+      self
+    end
+
+    def on_success(&block)
+      if success
+        block.call
+      end
+
+      self
+    end
+
+  end
+
+  module InstanceMethods
+    def retry_harder(options, &block)
+      times_to_run = options.delete(:times) || 1
+      wait_time = options.delete(:wait_time) || 0.5
+
+      times_to_run.times do
+        begin 
+          block.call
+          @success = true
+          break
+        rescue => e
+          sleep wait_time
+          @success = false
+        end
+      end
+
+      ::RetryHarder::RetryResult.new(:success => @success)
+    end
+  end
+
+  def self.initialize
+    Object.send(:include, RetryHarder::InstanceMethods)
+  end
+
+end
+
+RetryHarder.initialize
+
+puts "BEGIN"
+
+retry_harder(:times => 4) do
+  puts "harder!"
+
+  #raise
+
+end.on_failure do
+  puts "fail"
+end.on_success do
+  puts "winning"
+end
+
+puts "DONE"
